316cef77ceef03922ec6dea08456111c7ad6b791
Breno Gabriel <bbbgab123@gmail.com>
2024-06-08 23:38:31 -0300

    Feat: Chart and Button to hide ESP32

--- src/components/Log/index.tsx
+++ src/components/Log/index.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react';
-import styles from './index.module.css';
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
+import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 
 const useESPIPforConection = false;
 
 function Log() {
   const [data, setData] = useState({});
-  const [isConnected, setIsConnected] = useState(false);
+  const [, setIsConnected] = useState(true);
 
   const [isOpen, setIsOpen] = useState(true);
 
-  const ESP32_HOSTNAME = 'esp32.local';
+  const ESP32_HOSTNAME = "esp32.local";
   // Substitua pelo IP real do seu ESP32
-  const ESP32_IP = '192.168.176.19';
+  const ESP32_IP = "192.168.176.19";
 
   useEffect(() => {
     let gateway: string;
-    if(useESPIPforConection) {
+    if (useESPIPforConection) {
       gateway = `ws://${ESP32_IP}/ws`;
     } else {
       gateway = `ws://${ESP32_HOSTNAME}/ws`;
@@ -27,12 +28,12 @@ function Log() {
       websocket = new WebSocket(gateway);
 
       websocket.onopen = () => {
-        console.log('Conectado ao WebSocket');
+        console.log("Conectado ao WebSocket");
         setIsConnected(true);
       };
 
       websocket.onclose = () => {
-        console.log('Desconectado do WebSocket');
+        console.log("Desconectado do WebSocket");
         setIsConnected(false);
 
         // Tenta reconectar após 2 segundos
@@ -40,12 +41,12 @@ function Log() {
       };
 
       websocket.onmessage = (event) => {
-        console.log('Mensagem recebida:', event.data);
+        console.log("Mensagem recebida:", event.data);
         setData(JSON.parse(event.data));
       };
 
       websocket.onerror = (error) => {
-        console.error('WebSocket Error:', error);
+        console.error("WebSocket Error:", error);
       };
     };
 
@@ -59,18 +60,24 @@ function Log() {
   }, [ESP32_HOSTNAME]);
 
   return (
-    <div className={styles.logContainer}>
-      {
-        isOpen
-        &&
+    <div className={styles.logMainContainer}>
+      <div className={styles.logContainer}>
+        {isOpen && (
           <header className={styles.contentContainer}>
             <h3>ESP32 WebSocket Data</h3>
             <pre>{JSON.stringify(data, null, 2)}</pre>
           </header>
-      }
-      
+        )}
+      </div>
+
       <a className={styles.button} onClick={() => setIsOpen(!isOpen)}>
-        O
+        <div className={styles.modalOpenIcon}>
+          {isOpen ? (
+            <CaretRight size={32}></CaretRight>
+          ) : (
+            <CaretLeft size={32}></CaretLeft>
+          )}
+        </div>
       </a>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,17 @@
+import React from 'react';
 import Log from '../../components/Log';
 import styles from './index.module.css';
 import MainContainer from './MainContainer';
 import SideContainer from './SideContainer';
 
-const Home = () => {
-    return (
-        <div className={ styles['home-container'] }>
-            <SideContainer />
-            <MainContainer />
-            <div>
-                <Log/>
-            </div>
-        </div>
-    )
-}
+const Home: React.FC = () => {
+  return (
+    <div className={styles['home-container']}>
+      <SideContainer />
+      <MainContainer />
+      <Log />
+    </div>
+  );
+};
 
 export default Home;
